33012be23ff4b1becb24a5ac726e5596090a4139
Claudio Romero <romerolrocha@gmail.com>
2020-07-25 12:26:34 -0300

    Initial Commit

---
+++ reactavancado-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '92ff826af7df269b268303117ce7f3b1'),
+    },
+  },
+});
---
+++ reactavancado-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fb387c13-5b4e-4825-bf1c-99ad7e3d4f1e'
+};
