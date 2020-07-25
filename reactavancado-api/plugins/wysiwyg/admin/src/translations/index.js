b5a36b244c4377cf4cafe2d5e379620d8fd5bfaf
Claudio Romero <romerolrocha@gmail.com>
2020-07-25 17:31:49 -0300

    Add CKEditor plugin

---
+++ reactavancado-api/plugins/wysiwyg/admin/src/translations/index.js
@@ -0,0 +1,19 @@
+import de from "./de.json";
+import en from "./en.json";
+import es from "./es.json";
+import fr from "./fr.json";
+import ptBR from "./pt-BR.json";
+import pt from "./pt.json";
+import uk from "./uk.json";
+
+const trads = {
+  de,
+  en,
+  es,
+  fr,
+  "pt-BR": ptBR,
+  pt,
+  uk,
+};
+
+export default trads;
